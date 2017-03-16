ba458faca30e1a051bb40b9c37f6f89902f2a335
karim garcia <karim@mesmidis.com>
2017-03-16 18:38:03 +0100

    last change

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -2,6 +2,9 @@ class MealsController < ApplicationController
 
   def index
     @today_meals = Meal.for_today
+    # pour les tests à supprimer
+    @yesterday_meals = Meal.for_yesterday
+    @tomorrow_meals = Meal.for_tomorrow
   end
 
   def show
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -10,4 +10,21 @@ class Meal < ApplicationRecord
       joins("INNER JOIN restaurants ON meals.id = restaurants.#{day_name}_meal_id").
       where("#{day_name}_meal_count > 0")
   end
+
+
+# pour les tests à supprimer
+scope :for_yesterday, -> do
+    day_yesterday = Date.yesterday.strftime('%A').downcase
+    select("meals.*, #{day_yesterday}_meal_count as today_count").
+      joins("INNER JOIN restaurants ON meals.id = restaurants.#{day_yesterday}_meal_id").
+      where("#{day_yesterday}_meal_count > 0")
+  end
+
+scope :for_tomorrow, -> do
+    day_tomorrow = Date.tomorrow.strftime('%A').downcase
+    select("meals.*, #{day_tomorrow}_meal_count as today_count").
+      joins("INNER JOIN restaurants ON meals.id = restaurants.#{day_tomorrow}_meal_id").
+      where("#{day_tomorrow}_meal_count > 0")
+  end
 end
+
