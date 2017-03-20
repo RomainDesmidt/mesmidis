96ed0f02a77a614e8035cb0ecb8c38497ab8b3eb
Thomas Goulet <t.gouletpro@gmail.com>
2017-03-20 18:40:43 +0100

    Add map in meals and home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :guideline]
   def home
+    @restaurants = Restaurant.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
+      marker.lat restaurant.latitude
+      marker.lng restaurant.longitude
+    end
   end
 end
