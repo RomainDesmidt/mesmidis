a75b0cc8d1c3ed703d93c45599f175c13c0888af
karim garcia <karim@mesmidis.com>
2017-03-21 12:20:17 +0100

    fonts

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,7 +1,7 @@
 # initializers/mailjet.rb
 Mailjet.configure do |config|
-  config.api_key = '2ef3e9c0f8c4f60b68b6bdf0f891c278'
-  config.secret_key = 'ee00562f06614a40777ec7b4a9ab5d0e
+  config.api_key = ''
+  config.secret_key = '
 '
-  config.default_from = 'karim@mesmidis.com'
+  config.default_from = ''
 end
