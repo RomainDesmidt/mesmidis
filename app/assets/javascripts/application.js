e5d0b18f188a1c79cedb970f616503d61d76d142
karim garcia <karim@mesmidis.com>
2017-03-24 18:22:35 +0100

    add sweet alert2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,10 @@
+
 //= require jquery
 //= require jquery_ujs
 //= require materialize
 //= require underscore
 //= require gmaps/google
+//= require sweetalert2
+//= require sweet-alert2-rails
 //= require_tree .
 
