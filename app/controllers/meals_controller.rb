642fe962400c261bead00ca513abf0ccc2093e5d
Thomas Goulet <t.gouletpro@gmail.com>
2017-03-27 20:05:02 +0200

    Add functionnal matrix api

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -10,7 +10,22 @@ class MealsController < ApplicationController
       marker.infowindow "#{meal.restaurant.name}"
       #marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
     end
+
+    unless @meal_reserved.nil?
+      gmaps = GoogleMapsService::Client.new
+
+      origins =  [47.2161625, -1.5526961]
+      destinations = [@meal_reserved.restaurant.latitude, @meal_reserved.restaurant.longitude]
+      matrix = gmaps.distance_matrix(origins, destinations,
+        mode: 'walking',
+        language: 'fr-FR',
+        units: 'km')
+
+      @distance = matrix[:rows].first[:elements].first[:distance][:text]
+      @duration = matrix[:rows].first[:elements].first[:duration][:text]
+    end
   end
+
   def show
   end
 end
