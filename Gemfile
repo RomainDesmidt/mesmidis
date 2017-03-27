5140195d4b03d3d2b79df84fe273cf2ec67d9421
Thomas Goulet <t.gouletpro@gmail.com>
2017-03-27 16:34:12 +0200

    Add stripe functionnal

--- Gemfile
+++ Gemfile
@@ -22,8 +22,7 @@ gem "gmaps4rails"
 gem 'rails-assets-sweetalert2', '~> 5.1.1', source: 'https://rails-assets.org'
 gem 'sweet-alert2-rails'
 gem 'money-rails', '~>1'
-gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
-
+gem 'stripe'
 
 gem 'enumerize'
 
--- app/controllers/user_subscriptions_controller.rb
+++ app/controllers/user_subscriptions_controller.rb
@@ -9,22 +9,55 @@ class UserSubscriptionsController < ApplicationController
   end
 
   def create
-
-    @user_subscription = UserSubscription.new(user_subscription_params)
+    @user_subscription              = UserSubscription.new(user_subscription_params)
     @user_subscription.subscription = Subscription.find(params[:user_subscription][:subscription_id])
-    @user_subscription.user = current_user
+    @user_subscription.user         = current_user
+    @user_subscription.status       = "pending"
+
     @user_subscription.starting_on = Date.today
 
+    unless @user_subscription.save
+      redirect_to new_user_subscription_path
+    end
 
-    @user_subscription.save
+
+
+    unless current_user.stripe_customer_id
+      stripe_customer = Stripe::Customer.create(
+        source: params[:stripeToken],
+        email:  params[:stripeEmail]
+      )
+      current_user.update(stripe_customer_id: stripe_customer.id)
+    end
+
+    mapping_app_subscriptions_to_stripe_subscriptions = {
+      "L'avant-goût" => "Sub1",
+      "Le Sublime" => "Sub2",
+      "Le Gargantuesque" => "Sub3"
+    }
+
+    sub_name = @user_subscription.subscription.name
+    stripe_subscription = Stripe::Subscription.create(
+      :customer => current_user.stripe_customer_id,
+      :plan => mapping_app_subscriptions_to_stripe_subscriptions[sub_name]
+    )
+
+    @user_subscription.update(payment: stripe_subscription.to_json, status: "paid")
+    flash[:notice] = "Votre souscription a bien été enregistrée! Merci!"
+    redirect_to profile_path
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_user_subscription_path
   end
 
   def destroy
+
   end
 
   private
 
   def user_subscription_params
-    params.require(:user_subscription).permit(:user_id, :subscription_id, :description, :starting_on)
+    params.require(:user_subscription).permit(:user_id, :subscription_id, :description)
   end
 end
