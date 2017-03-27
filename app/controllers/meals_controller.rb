04458da6e1fbc1d42e649d2b93469797d0a6c88d
karim garcia <karim@mesmidis.com>
2017-03-27 19:09:48 +0200

    add news seeds modification index meals too

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -4,14 +4,26 @@ class MealsController < ApplicationController
     @order = Order.new
     # @filter_meals = Meal.find
     @meal_reserved = current_user.order_made_today
-    @hash = Gmaps4rails.build_markers(@today_meals) do |meal, marker,info|
-      marker.lat meal.restaurant.latitude
-      marker.lng meal.restaurant.longitude
-      marker.infowindow "#{meal.restaurant.name}"
-      #marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+
+    if @meal_reserved == nil
+      @hash = Gmaps4rails.build_markers(@today_meals) do |meal, marker,info|
+        marker.lat meal.restaurant.latitude
+        marker.lng meal.restaurant.longitude
+        marker.infowindow "#{meal.restaurant.name}"
+        #marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+      end
+    else
+      @hash = Gmaps4rails.build_markers(@meal_reserved) do |meal, marker,info|
+        marker.lat meal.restaurant.latitude
+        marker.lng meal.restaurant.longitude
+        marker.infowindow "#{meal.restaurant.name}"
+        #marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+
+      end
     end
+
   end
+
   def show
   end
 end
-
